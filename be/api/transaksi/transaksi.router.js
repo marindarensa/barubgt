c5ee74932f7428ad67141db9d12ff5df5726817f
marindarensa <62869131+marindarensa@users.noreply.github.com>
2022-03-08 10:20:08 +0700

    update

--- be/api/transaksi/transaksi.router.js
+++ be/api/transaksi/transaksi.router.js
@@ -5,9 +5,9 @@ const authorize = require("../auth/authorize");
 const { IsAdminKasir, IsAdmin, IsOwner } = require("../auth/role");
 
 // routes
-router.get("/", authorize, IsAdmin, controllerGetAll); //admin only
-router.get("/:id_transaksi", authorize, IsAdmin, controllerGetId); //admin only
+router.get("/", authorize, IsAdmin, controllerGetAll); //admin 
+router.get("/:id_transaksi", authorize, IsAdmin, IsAdminKasir, IsOwner, controllerGetId); //admin 
 router.post("/", authorize, IsAdminKasir, controllerAdd); // kasir
 router.put("/", authorize, IsAdminKasir, controllerEdit); //kasir
-router.delete("/", authorize, IsAdmin, controllerDelete); //admin only
+router.delete("/", authorize, IsAdminKasir, controllerDelete); //admin 
 module.exports = router;
