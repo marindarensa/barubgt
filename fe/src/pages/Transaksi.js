f573b1285a50739937a3374e315acb0c987e42cf
marindarensa <62869131+marindarensa@users.noreply.github.com>
2022-03-08 11:26:22 +0700

    update

--- fe/src/pages/Transaksi.js
+++ fe/src/pages/Transaksi.js
@@ -5,24 +5,70 @@ import TransaksiList from "../component/TransaksiList"
 export default class Transaksi extends React.Component {
     constructor() {
         super()
+        this.state = {
+            token: "",
+            transaksi: [],
+            // paket: [],
+            selectedItem: null
+        }
+
+        /* logika if-else --> untuk mengecek apakah user yg mengakses telah melakukan
+           login sebagai admin atau belum
+        */
+        if (localStorage.getItem("token")) {
+            this.state.token = localStorage.getItem("token")
+        } else {
+            window.location = "/login"
+        }
+
+        // header config -> untuk memberikan header berupa 'beare token' sebagai request API
+        // sebelum mengakses data
+        headerConfig = () => {
+            let header = {
+                headers: { Authorization: `Bearer ${this.state.token}` }
+            }
+            return header
+        }
+
+        // getTransaksi -> untuk mengakses API get transaksi
+        getTransaksi = () => {
+            let url = base_url + "/transaksi"
+            axios.get(url, this.headerConfig())
+                .then(response => {
+                    this.setState({ transaksi: response.data })
+                })
+                .catch(error => {
+                    if (error.response) {
+                        if (error.response.status) {
+                            window.alert(error.response.data.message)
+                            this.props.history.push("/login")
+                        }
+                    } else {
+                        console.log(error);
+                    }
+                })
+        }
     }
+    componentDidMount() {
+        this.getTransaksi()
+    }
+
     render() {
         return (
             <div>
                 <Navbar />
-
                 <div className="container">
-                    <h3 className="text-bold text-info mt-2">Transactions List</h3>
-                   
+                    <h3 className="text-bold text-info mt-2">Transaksi List</h3>
+                    {this.state.transaksi.map(item => (
                         <TransaksiList
-                        // key = {item.transaksi_id}
-                        // transaction_id = {item.transaksi_id}
-                        // customer_name = {item.customer.name}
-                        // customer_address = {item.customer.address}
-                        // time = {item.waktu}
-                        // products = {item.detail_transaksi}
-                         />
-                  
+                            key={item.id_transaksi}
+                            id_transaksi={item.id_transaksi}
+                            nama_member={item.member.nama_member}
+                            alamat={item.member.alamat}
+                            tgl={item.tgl}
+                            paket={item.detail_transaksi}
+                        />
+                    ))}
                 </div>
             </div>
         )
